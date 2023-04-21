3a2ff50825f3db2dbf6b340ead91d7bca99ab845
koketso17 <kktsmodise7@gmail.com>
2023-04-21 13:23:50 +0200

    mdb added

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'BL'`, () => {
+  it(`should have as title 'mdb-angular-ui-kit-free'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('BL');
+    expect(app.title).toEqual('mdb-angular-ui-kit-free');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('BL app is running!');
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('mdb-angular-ui-kit-free app is running!');
   });
 });
